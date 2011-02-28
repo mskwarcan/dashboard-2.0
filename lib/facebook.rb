42f0b7eb89afe6bb09d84adff7f6c8b8cd134ca0
Mike Skwarcan <mikeskwarcan@Mike-Skwarcans-iMac.local>
2011-02-28 16:47:47 -0600

    Workers

--- lib/facebook.rb
+++ lib/facebook.rb
@@ -1,29 +1,33 @@
 class Facebook < Struct.new(:user, :update, :heroku) 
   def perform
-    @update = Update.first(:conditions => {:id => update.id})
+    begin
+      @update = Update.first(:conditions => {:id => update.id})
     
-    facebook = User.facebook
+      facebook = User.facebook
     
-    page = facebook.selection.page(user.facebook_token)
+      page = facebook.selection.page(user.facebook_token)
     
-    @update.face_pic = page.picture
-    @update.face_name = page.info!["name"]
-    @update.likes = page.info!["likes"]
-    @update.new_likes = page.info!["likes"] - user.facebook_monthly_count
-    @update.feed = ActiveSupport::JSON.encode(page.feed.limit(10).info!["data"])
+      @update.face_pic = page.picture
+      @update.face_name = page.info!["name"]
+      @update.likes = page.info!["likes"]
+      @update.new_likes = page.info!["likes"] - user.facebook_monthly_count
+      @update.feed = ActiveSupport::JSON.encode(page.feed.limit(10).info!["data"])
     
-    @update.save
+      @update.save
     
-    page.feed.limit(10).info!["data"].each do |feed|
-      status = Feed.new
-      status.feed_id = feed["id"]
-      status.picture = facebook.selection.user(feed["from"]["id"]).picture
-      status.link = facebook.selection.user(feed["from"]["id"]).info!["link"]
+      page.feed.limit(10).info!["data"].each do |feed|
+        status = Feed.new
+        status.feed_id = feed["id"]
+        status.picture = facebook.selection.user(feed["from"]["id"]).picture
+        status.link = facebook.selection.user(feed["from"]["id"]).info!["link"]
       
-      status.save
+        status.save
+      end
+    rescue
+      return
     end
     
-    if Delayed::Job.count == 0
+    if Delayed::Job.count == 1
       heroku.set_workers(ENV["APP_NAME"], 0)
     end
   end    
--- lib/tweet.rb
+++ lib/tweet.rb
@@ -1,18 +1,23 @@
 class Tweet < Struct.new(:user, :update, :heroku) 
   def perform
-    @update = Update.first(:conditions => {:id => update.id})
+    begin
+      @update = Update.first(:conditions => {:id => update.id})
     
-    client = User.twitter(user)
+      client = User.twitter(user)
     
-    @update.twit_pic = client.info["profile_image_url"]
-    @update.twit_name = client.info["screen_name"]
-    @update.followers = client.info["followers_count"]
-    @update.new_followers = client.info["followers_count"] - user.twitter_monthly_count
-    @update.tweets = ActiveSupport::JSON.encode(client.user_timeline())
+      @update.twit_pic = client.info["profile_image_url"]
+      @update.twit_name = client.info["screen_name"]
+      @update.followers = client.info["followers_count"]
+      @update.new_followers = client.info["followers_count"] - user.twitter_monthly_count
+      @update.tweets = ActiveSupport::JSON.encode(client.user_timeline())
     
-    @update.save
+      @update.save
     
-    if Delayed::Job.count == 0
+    rescue
+      return
+    end
+    
+    if Delayed::Job.count == 1
       heroku.set_workers(ENV["APP_NAME"], 0)
     end
   end    
