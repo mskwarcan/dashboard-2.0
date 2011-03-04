266b6b2ab5609019558cbe05dd884f5385542719
Mike Skwarcan <mikeskwarcan@Mike-Skwarcans-iMac.local>
2011-03-04 10:38:31 -0600

    Top five

--- lib/mailchimp.rb
+++ lib/mailchimp.rb
@@ -8,9 +8,31 @@ class Mailchimp < Struct.new(:user, :update, :heroku)
       list_id = h.lists.first.second.first["id"]
       @update.chimp_name = h.lists.first.second.first["name"]
       @update.growth = ActiveSupport::JSON.encode(h.list_growth_history(list_id))
-      @update.campaign = ActiveSupport::JSON.encode(h.campaigns["data"].first)
+      @update.campaign = ActiveSupport::JSON.encode(h.campaigns["data"])
       @update.stats = ActiveSupport::JSON.encode(h.campaign_stats(h.campaigns["data"].first["id"]))
+      @update.stats2 = ActiveSupport::JSON.encode(h.campaign_stats(h.campaigns["data"].second["id"]))
+      @update.stats3 = ActiveSupport::JSON.encode(h.campaign_stats(h.campaigns["data"].third["id"]))
       @update.chatter = ActiveSupport::JSON.encode(h.chimp_chatter)
+      
+      click_rate = []
+      open_rate = []
+      
+      h.campaigns["data"].each do |campaign|
+        opened = h.campaign_stats(campaign["id"])["unique_opens"]
+        total = h.campaign_stats(campaign["id"])["emails_sent"]
+        clicks = h.campaign_stats(campaign["id"])["users_who_clicked"]
+        open = sprintf("%.2f", opened/total*100)
+        click = sprintf("%.2f", clicks/total*100)
+        
+        open_rate << {:rate => open, :name => campaign["title"], :opened => opened}
+        click_rate << {:rate => click, :name => campaign["title"], :clicks => clicks}
+      end
+      
+      open_rate.sort! { |a,b| a[:rate] <=> b[:rate] }
+      click_rate.sort! { |a,b| a[:rate] <=> b[:rate] }
+      
+      @update.top_open = ActiveSupport::JSON.encode(open_rate)
+      @update.top_click = ActiveSupport::JSON.encode(click_rate)
       
       @update.chimp_done = true
     
