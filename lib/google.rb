3dc64ff7031dfbb18a276f8219d5994e3c023ae7
Mike Skwarcan <mikeskwarcan@Mike-Skwarcans-iMac.local>
2011-03-08 12:43:30 -0600

    Ad words

--- lib/google.rb
+++ lib/google.rb
@@ -13,13 +13,13 @@ class Google < Struct.new(:user, :update, :heroku)
     	monthThree = 3.month.ago.strftime("%Y-%m-01")
     	nextMonth = 1.month.from_now.strftime("%Y-%m-01")
     
-      current = gs.get({:start_date => currentMonth, :end_date => nextMonth, :metrics => ['pageviews','avgTimeOnSite','newVisits','visits', 'goalCompletionsAll']})
+      current = gs.get({:start_date => currentMonth, :end_date => nextMonth, :metrics => ['pageviews','avgTimeOnSite','newVisits','visits', 'goalCompletionsAll', 'impressions','adClicks']})
       @update.results = ActiveSupport::JSON.encode(current.points.first.metrics)
-      last = gs.get({:start_date => lastMonth, :end_date => currentMonth, :metrics => ['pageviews','avgTimeOnSite','newVisits','visits', 'goalCompletionsAll']})
+      last = gs.get({:start_date => lastMonth, :end_date => currentMonth, :metrics => ['pageviews','avgTimeOnSite','newVisits','visits', 'goalCompletionsAll', 'impressions','adClicks']})
       @update.last_month = ActiveSupport::JSON.encode(last.points.first.metrics)
-      two_months = gs.get({:start_date => monthTwo, :end_date => lastMonth, :metrics => ['pageviews','avgTimeOnSite','newVisits','visits', 'goalCompletionsAll']})
+      two_months = gs.get({:start_date => monthTwo, :end_date => lastMonth, :metrics => ['pageviews','avgTimeOnSite','newVisits','visits', 'goalCompletionsAll', 'impressions','adClicks']})
       @update.two_months = ActiveSupport::JSON.encode(two_months.points.first.metrics)
-      three_months = gs.get({:start_date => monthThree, :end_date => monthTwo, :metrics => ['pageviews','avgTimeOnSite','newVisits','visits', 'goalCompletionsAll']})
+      three_months = gs.get({:start_date => monthThree, :end_date => monthTwo, :metrics => ['pageviews','avgTimeOnSite','newVisits','visits', 'goalCompletionsAll', 'impressions','adClicks']})
       @update.three_months = ActiveSupport::JSON.encode(three_months.points.first.metrics)
     
       @update.google_done = true
