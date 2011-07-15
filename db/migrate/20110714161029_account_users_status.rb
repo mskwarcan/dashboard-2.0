3e3302dcf8af6f805cf6113347768b61ceaf1929
Mike Skwarcan <mikeskwarcan@Mike-Skwarcans-iMac.local>
2011-07-15 15:08:51 -0500

    migrations

--- db/migrate/20110714161029_account_users_status.rb
+++ db/migrate/20110714161029_account_users_status.rb
@@ -1,6 +1,6 @@
 class AccountUsersStatus < ActiveRecord::Migration
   def self.up
-    add_column :accounts_users, :status, :integer, :default => 'pending'
+    add_column :accounts_users, :status, :string, :default => 'pending'
   end
 
   def self.down
